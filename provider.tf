dc2083cc0109937274ed7b3b9be8f81d9bfce970
testlab <testmail@ow.com>
2024-12-14 07:27:57 +0000

    adding provider.tf

---
+++ provider.tf
@@ -0,0 +1,6 @@
+provider "google" {
+  project     = "platinum-logic-426017-v3"
+  region      = "us-central1"
+  credentials = file("/home/devendra/gcp/base/platinum-logic-426017-v3-ebb1ab63c365.json")
+}
+
